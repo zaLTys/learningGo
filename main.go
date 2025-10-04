c006f1c6332dd71267409ae42f6e5b4475f75ef6
Povilas Simanskas <povilas.simanskas@vinted.com>
2025-10-04 13:13:47 +0300

    single user only

--- main.go
+++ main.go
@@ -4,21 +4,40 @@ import "fmt"
 
 func main() {
 
-	conferenceName : = "Go Conference"
+	conferenceName := "Go Conference"
 	const conferenceTickets = 50
-	var remainingTickets = 50
+	var remainingTickets uint = 50
 
 	fmt.Printf("confName is %T , confTickets is %T", conferenceName, conferenceTickets)
 	fmt.Printf("Welcome to %v\n", conferenceName)
 	fmt.Printf("Total %v, tickets remaining: %v\n", conferenceTickets, remainingTickets)
 	fmt.Println("Get your tickets here ")
 
-	var userName string
-	var userTickets int
+	var firstName string
+	var lastName string
+	var email string
+	var userTickets uint
 
-	userName = "Povilas"
-	userTickets = 5
+	fmt.Println("Pointer example")
+	fmt.Println(remainingTickets)
+	fmt.Println(&remainingTickets)
 
-	fmt.Printf("User %v has %v tickets\n", userName, userTickets)
+	fmt.Println("Enter your first name:")
+	fmt.Scanln(&firstName)
+	fmt.Println("Enter your last name:")
+	fmt.Scanln(&lastName)
+	fmt.Println("Enter your email:")
+	fmt.Scanln(&email)
+	fmt.Println("Enter number of tickets:")
+	fmt.Scanln(&userTickets)
 
+	var bookings []string
+	bookings = append(bookings, firstName+"  "+lastName)
+
+	remainingTickets = remainingTickets - userTickets
+
+	fmt.Printf("Whole array %v\n", bookings)
+
+	fmt.Printf("Thans %v %v for reserving %v tickets on email %v\n", firstName, lastName, userTickets, email)
+	fmt.Printf("Total %v, tickets remaining: %v\n", conferenceTickets, remainingTickets)
 }
